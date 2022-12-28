b39a926ea0cdd79b41eb23f2578c46a01cac3cca
unknown <hiteshpatil912@gmail.com>
2022-12-28 12:12:11 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+
+createApp(App).mount('#app')
+
+
